02e3c37a451b78b3ecbb2308f659f175b9cf8fd9
Brenda <bee@bee.com>
2022-06-21 14:57:27 +0300

    Outlines the Profile model.

--- majirani/models.py
+++ majirani/models.py
@@ -12,3 +12,9 @@ class Neighborhood(models.Model):
     pub_date = models.DateTimeField(auto_now_add=True)
     Admin = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
 
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    photo = models.ImageField(upload_to = 'profile_pics/', blank=True, default='profile_pics/default.jpg')
+    neighborhood = models.ForeignKey(Neighborhood,on_delete=models.CASCADE, blank=True, default='1')
