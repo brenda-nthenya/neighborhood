e865969be1956d398c80d0fc8de6ff964a742a26
Brenda <bee@bee.com>
2022-06-21 15:34:49 +0300

    Adds the delete, and class Meta

--- majirani/models.py
+++ majirani/models.py
@@ -61,3 +61,13 @@ class Profile(models.Model):
             output_size = (300, 300)
             img.thumbnail(output_size)
             img.save(self.photo.path)
+
+    def delete_profile(self):
+        self.delete()
+    
+    def __str__(self):
+        return f"{self.user}, {self.bio}, {self.photo}"
+    
+    class Meta:
+        verbose_name = 'Profile'
+        verbose_name_plural = 'Profiles'
