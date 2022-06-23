0f86896e4c0369159ec2344a7e6740762d4fcd11
Brenda <bee@bee.com>
2022-06-23 19:07:49 +0300

    Creates a Newsletter form

--- majirani/forms.py
+++ majirani/forms.py
@@ -38,3 +38,7 @@ class UserUpdateForm(forms.ModelForm):
       class Meta:
         model = User
         fields = ['username','email'] 
+
+class NewsLetterForm(forms.Form):
+    your_name = forms.CharField(label='First Name',max_length=30)
+    email = forms.EmailField(label='Email')
